47157d12e5a5ee5a04efe81ab4c7e75bf9c62f29
diviz <diviz.dev@gmail.com>
2018-04-07 17:53:43 +0300

    first commit

--- src/main/java/ru/divizdev/Main.java
+++ src/main/java/ru/divizdev/Main.java
@@ -10,11 +10,10 @@ public class Main {
         // Initialize Api Context
         ApiContextInitializer.init();
         System.out.print("Start");
-        // TODO Instantiate Telegram Bots API
+
         TelegramBotsApi botsApi = new TelegramBotsApi();
 
 
-        // TODO Register our bot
 
         try {
             botsApi.registerBot(new ChatBotPasswordRequest());
